95407897ced414c03057343853896ae9cc05e243
rala72 <code@rala.io>
2019-11-08 22:21:39 +0100

    initial commit

---
+++ src/main/java/io/rala/math/Vector.java
@@ -0,0 +1,323 @@
+package io.rala.math;
+
+/**
+ * class which holds a vector with x &amp; y
+ */
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class Vector implements Comparable<Vector> {
+    // region attributes
+    private double x;
+    private double y;
+    // endregion
+
+    // region constructors
+
+    /**
+     * calls {@link #Vector(double)} with <code>0</code>
+     *
+     * @see #Vector(double)
+     * @see #Vector(double, double)
+     */
+    public Vector() {
+        this(0);
+    }
+
+    /**
+     * calls {@link #Vector(double, double)} with the value at x and y
+     *
+     * @param xy value to be used in {@link #Vector(double, double)} at x and y
+     * @see #Vector()
+     * @see #Vector(double, double)
+     */
+    public Vector(double xy) {
+        this(xy, xy);
+    }
+
+    /**
+     * creates a vector with given x and y values
+     *
+     * @param x x value of vector
+     * @param y y value of vector
+     * @see #Vector()
+     * @see #Vector(double)
+     */
+    public Vector(double x, double y) {
+        setX(x);
+        setY(y);
+    }
+    // endregion
+
+    // region getter and setter
+
+    /**
+     * @return x value of vector
+     */
+    public double getX() {
+        return x;
+    }
+
+    /**
+     * @param x new x value of vector
+     */
+    public void setX(double x) {
+        this.x = !Double.isFinite(x) ? 0 : x;
+    }
+
+    /**
+     * @return y value of vector
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * @param y new y value of vector
+     */
+    public void setY(double y) {
+        this.y = !Double.isFinite(y) ? 0 : y;
+    }
+
+    /**
+     * @param xy new x and y value of vector
+     */
+    public void setXY(double xy) {
+        setX(xy);
+        setY(xy);
+    }
+    // endregion
+
+    // region length, add, subtract and multiply
+
+    /**
+     * @return length of vector based on pythagoras
+     */
+    public double length() {
+        return Math.sqrt(Math.pow(getX(), 2) + Math.pow(getY(), 2));
+    }
+
+    /**
+     * calls {@link #add(double, double)} with given value
+     *
+     * @param xy x and y value to add
+     * @return new vector with sum of current and given parameters
+     * @see #add(double, double)
+     * @see #add(Vector)
+     */
+    public Vector add(double xy) {
+        return add(xy, xy);
+    }
+
+    /**
+     * calls {@link #add(Vector)} with given values
+     *
+     * @param x x value to add
+     * @param y y value to add
+     * @return new vector with sum of current and given parameters
+     * @see #add(double)
+     * @see #add(Vector)
+     */
+    public Vector add(double x, double y) {
+        return add(new Vector(x, y));
+    }
+
+    /**
+     * @param vector vector value to add
+     * @return new vector with sum of current and given vector
+     * @see #add(double)
+     * @see #add(double, double)
+     */
+    public Vector add(Vector vector) {
+        return new Vector(getX() + vector.getX(), getY() + vector.getY());
+    }
+
+    /**
+     * calls {@link #subtract(double, double)} with given value
+     *
+     * @param xy x and y value to subtract
+     * @return new vector with difference of current and given parameters
+     * @see #subtract(double, double)
+     * @see #subtract(Vector)
+     */
+    public Vector subtract(double xy) {
+        return subtract(xy, xy);
+    }
+
+    /**
+     * calls {@link #subtract(Vector)} with given values
+     *
+     * @param x x value to subtract
+     * @param y y value to subtract
+     * @return new vector with difference of current and given parameters
+     * @see #subtract(double)
+     * @see #subtract(Vector)
+     */
+    public Vector subtract(double x, double y) {
+        return subtract(new Vector(x, y));
+    }
+
+    /**
+     * @param vector vector value to subtract
+     * @return new vector with difference of current and given vector
+     * @see #subtract(double)
+     * @see #subtract(double, double)
+     */
+    public Vector subtract(Vector vector) {
+        return add(vector.inverse());
+    }
+
+    /**
+     * @param i value to multiply with x &amp; y
+     * @return new vector with multiplied x &amp; y values
+     */
+    public Vector multiply(double i) {
+        return new Vector(getX() * i, getY() * i);
+    }
+    // endregion
+
+    // region inverse
+
+    /**
+     * @return new vector which has inverse x &amp; y values
+     * @see #inverseX()
+     * @see #inverseY()
+     */
+    public Vector inverse() {
+        return inverseX().inverseY();
+    }
+
+    /**
+     * @return new vector which has inverse x value
+     * @see #inverse()
+     * @see #inverseY()
+     */
+    public Vector inverseX() {
+        return new Vector(-getX(), getY());
+    }
+
+    /**
+     * @return new vector which has inverse y value
+     * @see #inverse()
+     * @see #inverseX()
+     */
+    public Vector inverseY() {
+        return new Vector(getX(), -getY());
+    }
+    // endregion
+
+    // region rotate, normalized and scalarProduct
+
+    /**
+     * @param phi angle in radiant
+     * @return new vector rotated by given angle
+     */
+    public Vector rotate(double phi) {
+        double newX = Math.cos(phi) * getX() - Math.sin(phi) * getY();
+        double newY = Math.sin(phi) * getX() + Math.cos(phi) * getY();
+        return new Vector(newX, newY);
+    }
+
+    /**
+     * @return new normalized vector
+     */
+    public Vector normalized() {
+        return new Vector(getX() / length(), getY() / length());
+    }
+
+    /**
+     * @param vector to calc scalar product
+     * @return scalar product
+     */
+    public double scalarProduct(Vector vector) {
+        return getX() * vector.getX() + getY() * vector.getY();
+    }
+    // endregion
+
+    // region round
+
+    /**
+     * @return new vector with absolute values
+     * @see Math#abs(double)
+     */
+    public Vector absolute() {
+        return new Vector(Math.abs(getX()), Math.abs(getY()));
+    }
+
+    /**
+     * @return new vector with {@link Math#round(double)}-values
+     * @see #floor()
+     * @see #ceil()
+     */
+    public Vector round() {
+        return new Vector(Math.round(getX()), Math.round(getY()));
+    }
+
+    /**
+     * @return new vector with {@link Math#floor(double)}-values
+     * @see #round()
+     * @see #ceil()
+     */
+    public Vector floor() {
+        return new Vector(Math.floor(getX()), Math.floor(getY()));
+    }
+
+    /**
+     * @return new vector with {@link Math#ceil(double)}-values
+     * @see #round()
+     * @see #floor()
+     */
+    public Vector ceil() {
+        return new Vector(Math.ceil(getX()), Math.ceil(getY()));
+    }
+
+
+    /**
+     * @return new vector with truncated-values
+     * @see #round()
+     * @see #floor()
+     * @see #ceil()
+     */
+    public Vector truncate() {
+        return new Vector((long) getX(), (long) getY());
+    }
+    // endregion
+
+    // region isZeroVector and copy
+
+    /**
+     * @return <code>true</code> if both params casted to <code>int</code> are zero
+     */
+    public boolean isZeroVector() {
+        return (int) getX() == 0 && (int) getY() == 0;
+    }
+
+    /**
+     * @return new vector with same values
+     */
+    public Vector copy() {
+        return new Vector(getX(), getY());
+    }
+    // endregion
+
+    // region override
+
+    @Override
+    public String toString() {
+        return getX() + ":" + getY();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Vector)) return false;
+        Vector vector = (Vector) obj;
+        return getX() == vector.getX() && getY() == vector.getY();
+    }
+
+    @Override
+    public int compareTo(Vector v) {
+        int diffX = (int) Math.ceil(getX() - v.getX());
+        if (diffX != 0) return diffX;
+        return (int) Math.ceil(getY() - v.getY());
+    }
+    // endregion
+}
---
+++ src/test/java/io/rala/math/VectorTest.java
@@ -0,0 +1,199 @@
+package io.rala.math;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class VectorTest {
+    @Test
+    void testConstructors() {
+        assertVector(new Vector());
+        assertVector(new Vector(1), 1);
+        assertVector(new Vector(2, 2), 2);
+        assertVector(new Vector(2, 3), 2, 3);
+    }
+
+    @Test
+    void testGetterAndSetter() {
+        Vector vector = new Vector();
+        assertVector(vector);
+        vector.setX(1);
+        assertVector(vector, 1, 0);
+        vector.setY(2);
+        assertVector(vector, 1, 2);
+        vector.setXY(3);
+        assertVector(vector, 3);
+    }
+
+    @Test
+    void testLength() {
+        Assertions.assertEquals(0, new Vector().length());
+        Assertions.assertEquals(1.4142135623730951, new Vector(1).length());
+        Assertions.assertEquals(1, new Vector(1, 0).length());
+    }
+
+    @Test
+    void testAdd() {
+        Vector vector2 = new Vector(2, 2);
+        Assertions.assertEquals(vector2, new Vector().add(2));
+        Assertions.assertEquals(vector2, new Vector(1).add(1, 1));
+        Assertions.assertEquals(vector2, new Vector(1, 0).add(1, 2));
+    }
+
+    @Test
+    void testSubtract() {
+        Vector vector2 = new Vector(2, 2);
+        Assertions.assertEquals(new Vector(), vector2.subtract(2));
+        Assertions.assertEquals(new Vector(1), vector2.subtract(1, 1));
+        Assertions.assertEquals(new Vector(1, 0), vector2.subtract(1, 2));
+    }
+
+    @Test
+    void testMultiply() {
+        Assertions.assertEquals(new Vector(), new Vector().multiply(1));
+        Assertions.assertEquals(new Vector(), new Vector(1).multiply(0));
+        Assertions.assertEquals(new Vector(1), new Vector(1).multiply(1));
+        Assertions.assertEquals(new Vector(-2, -1), new Vector(2, 1).multiply(-1));
+    }
+
+    @Test
+    void testInverse() {
+        Assertions.assertEquals(new Vector(-1, 2), new Vector(1, 2).inverseX());
+        Assertions.assertEquals(new Vector(1, -2), new Vector(1, 2).inverseY());
+        Assertions.assertEquals(new Vector(-1, -2), new Vector(1, 2).inverse());
+    }
+
+    @Test
+    void testRotate() {
+        Vector vector = new Vector(1, 2);
+        assertVector(vector.rotate(Math.PI / 2), -2, 1.0000000000000002);
+        assertVector(vector.rotate(Math.PI), -1.0000000000000002, -1.9999999999999998);
+        assertVector(vector.rotate(Math.PI * 3 / 2), 1.9999999999999998, -1.0000000000000004);
+        assertVector(vector.rotate(Math.PI * 2), 1.0000000000000004, 1.9999999999999998);
+    }
+
+    @Test
+    void testNormalized() {
+        Vector vector0 = new Vector().normalized();
+        assertVector(vector0);
+        Assertions.assertEquals(0, vector0.length());
+
+        Vector vector1 = new Vector(1).normalized();
+        assertVector(vector1, 0.7071067811865475);
+        Assertions.assertEquals(1, Math.round(vector1.length()));
+
+        Vector vector2 = new Vector(1, 2).normalized();
+        assertVector(vector2, 0.4472135954999579, 0.8944271909999159);
+        Assertions.assertEquals(1, Math.round(vector2.length()));
+    }
+
+    @Test
+    void testScalarProduct() {
+        Assertions.assertEquals(0, new Vector(0).scalarProduct(new Vector(1)));
+        Assertions.assertEquals(2, new Vector(1).scalarProduct(new Vector(1)));
+        Assertions.assertEquals(6, new Vector(2).scalarProduct(new Vector(1, 2)));
+    }
+
+    @Test
+    void testAbsolute() {
+        Assertions.assertEquals(new Vector(1, 2), new Vector(-1, 2).absolute());
+        Assertions.assertEquals(new Vector(1, 2), new Vector(-1, -2).absolute());
+        Assertions.assertEquals(new Vector(1, 2), new Vector(1, -2).absolute());
+        Assertions.assertEquals(new Vector(1, 2), new Vector(1, 2).absolute());
+    }
+
+    @Test
+    void testRound() {
+        Assertions.assertEquals(new Vector(1, -2), new Vector(1, -2).round());
+        Assertions.assertEquals(new Vector(1, -2), new Vector(1.2, -2.2).round());
+        Assertions.assertEquals(new Vector(1, -2), new Vector(1.49, -2.49).round());
+        Assertions.assertEquals(new Vector(2, -2), new Vector(1.5, -2.5).round());
+        Assertions.assertEquals(new Vector(2, -3), new Vector(1.7, -2.7).round());
+    }
+
+    @Test
+    void testFloor() {
+        Assertions.assertEquals(new Vector(1, -2), new Vector(1, -2).floor());
+        Assertions.assertEquals(new Vector(1, -3), new Vector(1.2, -2.2).floor());
+        Assertions.assertEquals(new Vector(1, -3), new Vector(1.49, -2.49).floor());
+        Assertions.assertEquals(new Vector(1, -3), new Vector(1.5, -2.5).floor());
+        Assertions.assertEquals(new Vector(1, -3), new Vector(1.7, -2.7).floor());
+    }
+
+    @Test
+    void testCeil() {
+        Assertions.assertEquals(new Vector(1, -2), new Vector(1, -2).ceil());
+        Assertions.assertEquals(new Vector(2, -2), new Vector(1.2, -2.2).ceil());
+        Assertions.assertEquals(new Vector(2, -2), new Vector(1.49, -2.49).ceil());
+        Assertions.assertEquals(new Vector(2, -2), new Vector(1.5, -2.5).ceil());
+        Assertions.assertEquals(new Vector(2, -2), new Vector(1.7, -2.7).ceil());
+    }
+
+    @Test
+    void testTruncate() {
+        Assertions.assertEquals(new Vector(1, -2), new Vector(1, -2).truncate());
+        Assertions.assertEquals(new Vector(1, -2), new Vector(1.2, -2.2).truncate());
+        Assertions.assertEquals(new Vector(1, -2), new Vector(1.49, -2.49).truncate());
+        Assertions.assertEquals(new Vector(1, -2), new Vector(1.5, -2.5).truncate());
+        Assertions.assertEquals(new Vector(1, -2), new Vector(1.7, -2.7).truncate());
+    }
+
+    @Test
+    void testIsZeroVector() {
+        Assertions.assertTrue(new Vector().isZeroVector());
+        Assertions.assertFalse(new Vector(1).isZeroVector());
+    }
+
+    @Test
+    void testCopy() {
+        Vector vector = new Vector(2, 3);
+        Assertions.assertEquals(vector, vector.copy());
+    }
+
+    @Test
+    void testToString() {
+        Vector vector = new Vector(2, 3);
+        Assertions.assertEquals("2.0:3.0", vector.toString());
+    }
+
+    @Test
+    void testEquals() {
+        Vector vector = new Vector(2, 3);
+        Assertions.assertEquals(
+            vector,
+            new Vector(2, 3)
+        );
+        Assertions.assertNotEquals(
+            vector,
+            new Vector(3, 2)
+        );
+    }
+
+    @Test
+    void testCompareTo() {
+        Vector vector = new Vector(2, 3);
+        Assertions.assertEquals(
+            0, vector.compareTo(new Vector(2, 3))
+        );
+        Assertions.assertEquals(
+            -1,
+            0, vector.compareTo(new Vector(2, 1))
+        );
+        Assertions.assertEquals(
+            1,
+            0, vector.compareTo(new Vector(1, 0))
+        );
+    }
+
+    private static void assertVector(Vector vector) {
+        assertVector(vector, 0);
+    }
+
+    private static void assertVector(Vector vector, double xy) {
+        assertVector(vector, xy, xy);
+    }
+
+    private static void assertVector(Vector vector, double x, double y) {
+        Assertions.assertEquals(x, vector.getX());
+        Assertions.assertEquals(y, vector.getY());
+    }
+}
