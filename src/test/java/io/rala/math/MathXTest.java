da239c01a39b1c8bf7b913c6bd8cb02b571d7b0d
rala72 <code@rala.io>
2019-11-08 22:48:48 +0100

    move MathX arguments stream to ArgumentStreamFactory

--- src/test/java/io/rala/math/MathXTest.java
+++ src/test/java/io/rala/math/MathXTest.java
@@ -1,44 +1,22 @@
 package io.rala.math;
 
+import io.rala.math.testUtils.ArgumentStreamFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
 class MathXTest {
     @ParameterizedTest
-    @MethodSource("getFactorsOfArguments")
+    @MethodSource("getFactorsArguments")
     void testFactorsOf(int number, List<Integer> expected) {
         Assertions.assertEquals(expected, MathX.factors(number));
     }
 
-    private static Stream<Arguments> getFactorsOfArguments() {
-        return Stream.of(
-            Arguments.of(0, Collections.emptyList()),
-            Arguments.of(1, Collections.emptyList()),
-            Arguments.of(2, List.of(2)),
-            Arguments.of(3, List.of(3)),
-            Arguments.of(4, List.of(2, 2)),
-            Arguments.of(5, List.of(5)),
-            Arguments.of(6, List.of(2, 3)),
-            Arguments.of(7, List.of(7)),
-            Arguments.of(8, List.of(2, 2, 2)),
-            Arguments.of(9, List.of(3, 3)),
-            Arguments.of(10, List.of(2, 5)),
-            Arguments.of(11, List.of(11)),
-            Arguments.of(12, List.of(2, 2, 3)),
-            Arguments.of(13, List.of(13)),
-            Arguments.of(14, List.of(2, 7)),
-            Arguments.of(15, List.of(3, 5)),
-            Arguments.of(16, List.of(2, 2, 2, 2)),
-            Arguments.of(100, List.of(2, 2, 5, 5)),
-            Arguments.of(200, List.of(2, 2, 2, 5, 5)),
-            Arguments.of(1000, List.of(2, 2, 2, 5, 5, 5)),
-            Arguments.of(10000, List.of(2, 2, 2, 2, 5, 5, 5, 5))
-        );
+    private static Stream<Arguments> getFactorsArguments() {
+        return ArgumentStreamFactory.getMathXFactorsArguments();
     }
 }
