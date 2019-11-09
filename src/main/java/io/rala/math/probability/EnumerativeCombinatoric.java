7652f937e3f7c2852006314e3f0429a14629b9b3
rala72 <code@rala.io>
2019-11-09 13:56:06 +0100

    add BigInteger support to EnumerativeCombinatoric

--- src/main/java/io/rala/math/probability/EnumerativeCombinatoric.java
+++ src/main/java/io/rala/math/probability/EnumerativeCombinatoric.java
@@ -2,12 +2,15 @@ package io.rala.math.probability;
 
 import io.rala.math.MathX;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 
 @SuppressWarnings({"unused", "WeakerAccess"})
 public class EnumerativeCombinatoric {
     private EnumerativeCombinatoric() {
     }
+
+    // region permutations
 
     /**
      * @param n number of elements
@@ -24,6 +27,15 @@ public class EnumerativeCombinatoric {
      * @see MathX#factorial(long)
      */
     public static long permutationsWithoutRepetition(long n) {
+        return permutationsWithoutRepetition(BigInteger.valueOf(n)).longValueExact();
+    }
+
+    /**
+     * @param n number of elements
+     * @return <code>n!</code>
+     * @see MathX#factorial(BigInteger)
+     */
+    public static BigInteger permutationsWithoutRepetition(BigInteger n) {
         return MathX.factorial(n);
     }
 
@@ -36,7 +48,7 @@ public class EnumerativeCombinatoric {
      * @see MathX#factorial(int)
      */
     public static long permutationsWithRepetition(int n, int... k) {
-        return permutationsWithRepetition(n, Arrays.stream(k).mapToLong(MathX::factorial).toArray());
+        return permutationsWithRepetition(n, Arrays.stream(k).mapToLong(Long::valueOf).toArray());
     }
 
     /**
@@ -48,9 +60,29 @@ public class EnumerativeCombinatoric {
      * @see MathX#factorial(long)
      */
     public static long permutationsWithRepetition(long n, long... k) {
-        return MathX.factorial(n) / Arrays.stream(k).map(MathX::factorial)
-            .reduce((left, right) -> left * right).orElse(1);
+        return permutationsWithRepetition(BigInteger.valueOf(n),
+            Arrays.stream(k).mapToObj(BigInteger::valueOf).toArray(BigInteger[]::new)
+        ).longValueExact();
     }
+
+    /**
+     * <i>multinomial theorem</i>
+     *
+     * @param n number of elements
+     * @param k sub number of elements
+     * @return <code>n! / product(k)</code>
+     * @see MathX#factorial(BigInteger)
+     */
+    public static BigInteger permutationsWithRepetition(BigInteger n, BigInteger... k) {
+        return MathX.factorial(n).divide(
+            Arrays.stream(k).map(MathX::factorial)
+                .reduce(BigInteger::multiply).orElse(BigInteger.ONE)
+        );
+    }
+
+    // endregion
+
+    // region variations
 
     /**
      * @param n number of elements
@@ -69,14 +101,26 @@ public class EnumerativeCombinatoric {
      * @see MathX#factorial(long)
      */
     public static long variationsWithoutRepetition(long n, long k) {
-        return MathX.factorial(n) / MathX.factorial(n - k);
+        return variationsWithoutRepetition(
+            BigInteger.valueOf(n), BigInteger.valueOf(k)
+        ).longValueExact();
+    }
+
+    /**
+     * @param n number of elements
+     * @param k sub number of elements
+     * @return <code>nPr(n,k)</code>
+     * @see MathX#factorial(BigInteger)
+     */
+    public static BigInteger variationsWithoutRepetition(BigInteger n, BigInteger k) {
+        return MathX.factorial(n).divide(MathX.factorial(n.subtract(k)));
     }
 
     /**
      * @param n number of elements
      * @param k sub number of elements
      * @return n^k
-     * @see Math#pow(double, double)
+     * @see BigInteger#pow(int)
      */
     public static long variationsWithRepetition(int n, int k) {
         return variationsWithRepetition((long) n, k);
@@ -84,13 +128,29 @@ public class EnumerativeCombinatoric {
 
     /**
      * @param n number of elements
-     * @param k sub number of elements
+     * @param k sub number of elements - has to be in {@link Integer} range
      * @return n^k
-     * @see Math#pow(double, double)
+     * @see BigInteger#pow(int)
      */
     public static long variationsWithRepetition(long n, long k) {
-        return (long) Math.pow(n, k);
+        return variationsWithRepetition(
+            BigInteger.valueOf(n), BigInteger.valueOf(k)
+        ).longValueExact();
     }
+
+    /**
+     * @param n number of elements
+     * @param k sub number of elements - has to be in {@link Integer} range
+     * @return n^k
+     * @see BigInteger#pow(int)
+     */
+    public static BigInteger variationsWithRepetition(BigInteger n, BigInteger k) {
+        return n.pow(k.intValueExact());
+    }
+
+    // endregion
+
+    // region combinations
 
     /**
      * @param n number of elements
@@ -109,7 +169,19 @@ public class EnumerativeCombinatoric {
      * @see MathX#factorial(long)
      */
     public static long combinationsWithoutRepetition(long n, long k) {
-        return MathX.factorial(n) / (MathX.factorial(n - k) * MathX.factorial(k));
+        return combinationsWithoutRepetition(
+            BigInteger.valueOf(n), BigInteger.valueOf(k)
+        ).longValueExact();
+    }
+
+    /**
+     * @param n number of elements
+     * @param k sub number of elements
+     * @return <code>nCr(n,k)</code>
+     * @see MathX#factorial(BigInteger)
+     */
+    public static BigInteger combinationsWithoutRepetition(BigInteger n, BigInteger k) {
+        return MathX.factorial(n).divide(MathX.factorial(n.subtract(k)).multiply(MathX.factorial(k)));
     }
 
     /**
@@ -129,6 +201,18 @@ public class EnumerativeCombinatoric {
      * @see #combinationsWithoutRepetition(long, long)
      */
     public static long combinationsWithRepetition(long n, long k) {
-        return combinationsWithoutRepetition(n + k - 1, k);
+        return combinationsWithRepetition(BigInteger.valueOf(n), BigInteger.valueOf(k)).longValueExact();
     }
+
+    /**
+     * @param n number of elements
+     * @param k sub number of elements
+     * @return <code>nCr(n+k-1,k)</code>
+     * @see #combinationsWithoutRepetition(BigInteger, BigInteger)
+     */
+    public static BigInteger combinationsWithRepetition(BigInteger n, BigInteger k) {
+        return combinationsWithoutRepetition(n.add(k).subtract(BigInteger.ONE), k);
+    }
+
+    // endregion
 }
