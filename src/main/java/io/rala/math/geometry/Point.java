977a2860e1fcd90536a1d73c5df0a25d45cd5cc8
rala72 <code@rala.io>
2019-11-09 14:29:29 +0100

    add point class

---
+++ src/main/java/io/rala/math/geometry/Point.java
@@ -0,0 +1,121 @@
+package io.rala.math.geometry;
+
+import java.util.Objects;
+
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class Point implements Comparable<Point> {
+    // region attributes
+
+    private double x;
+    private double y;
+
+    // endregion
+
+    // region constructors
+
+    /**
+     * calls {@link #Point(double)} with <code>0</code>
+     *
+     * @see #Point(double)
+     * @see #Point(double, double)
+     */
+    public Point() {
+        this(0);
+    }
+
+    /**
+     * calls {@link #Point(double, double)} with the value at x and y
+     *
+     * @param xy value to be used in {@link #Point(double, double)} at x and y
+     * @see #Point()
+     * @see #Point(double, double)
+     */
+    public Point(double xy) {
+        this(xy, xy);
+    }
+
+    /**
+     * creates a vector with given x and y values
+     *
+     * @param x x value of vector
+     * @param y y value of vector
+     * @see #Point()
+     * @see #Point(double)
+     */
+    public Point(double x, double y) {
+        setX(x);
+        setY(y);
+    }
+
+    // endregion
+
+    // region getter and setter
+
+    /**
+     * @return x value of vector
+     */
+    public double getX() {
+        return x;
+    }
+
+    /**
+     * @param x new x value of vector
+     */
+    public void setX(double x) {
+        this.x = !Double.isFinite(x) ? 0 : x;
+    }
+
+    /**
+     * @return y value of vector
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * @param y new y value of vector
+     */
+    public void setY(double y) {
+        this.y = !Double.isFinite(y) ? 0 : y;
+    }
+
+    /**
+     * @param xy new x and y value of vector
+     */
+    public void setXY(double xy) {
+        setX(xy);
+        setY(xy);
+    }
+
+    // endregion
+
+    // region override
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(point.getX(), getX()) == 0 &&
+            Double.compare(point.getY(), getY()) == 0;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        int compare = Double.compare(getX(), o.getX());
+        if (compare != 0) return compare;
+        return Double.compare(getY(), o.getY());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY());
+    }
+
+    @Override
+    public String toString() {
+        return getX() + ":" + getY();
+    }
+
+    // endregion
+}
---
+++ src/test/java/io/rala/math/geometry/PointTest.java
@@ -0,0 +1,95 @@
+package io.rala.math.geometry;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PointTest {
+    // region constructors, getter and setter
+
+    @Test
+    void testConstructors() {
+        assertPoint(new Point());
+        assertPoint(new Point(1), 1);
+        assertPoint(new Point(2, 2), 2);
+        assertPoint(new Point(2, 3), 2, 3);
+    }
+
+    @Test
+    void testGetterAndSetter() {
+        Point point = new Point();
+        assertPoint(point);
+        point.setX(1);
+        assertPoint(point, 1, 0);
+        point.setY(2);
+        assertPoint(point, 1, 2);
+        point.setXY(3);
+        assertPoint(point, 3);
+    }
+
+    // endregion
+
+    // region override
+
+    @Test
+    void testToString() {
+        Point point = new Point(2, 3);
+        Assertions.assertEquals("2.0:3.0", point.toString());
+    }
+
+    @Test
+    void testEquals() {
+        Point point = new Point(2, 3);
+        Assertions.assertEquals(
+            point,
+            new Point(2, 3)
+        );
+        Assertions.assertNotEquals(
+            point,
+            new Point(3, 2)
+        );
+    }
+
+    @Test
+    void testHashCode() {
+        Assertions.assertEquals(
+            525249,
+            new Point(2, 3).hashCode()
+        );
+    }
+
+    @Test
+    void testCompareTo() {
+        Point point = new Point(2, 3);
+        Assertions.assertEquals(
+            0, point.compareTo(new Point(2, 3))
+        );
+        Assertions.assertEquals(
+            -1,
+            0, point.compareTo(new Point(2, 1))
+        );
+        Assertions.assertEquals(
+            1,
+            0, point.compareTo(new Point(1, 0))
+        );
+    }
+
+    // endregion
+
+
+    // region assert
+
+    private static void assertPoint(Point point) {
+        assertPoint(point, 0);
+    }
+
+    private static void assertPoint(Point point, double xy) {
+        assertPoint(point, xy, xy);
+    }
+
+    private static void assertPoint(Point point, double x, double y) {
+        Assertions.assertEquals(x, point.getX());
+        Assertions.assertEquals(y, point.getY());
+    }
+
+    // endregion
+}
