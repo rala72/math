29fc2787e2c9fb0490ab3f2ab2efcb05c52bb029
rala72 <code@rala.io>
2019-11-10 01:15:43 +0100

    add rect class

---
+++ src/main/java/io/rala/math/geometry/Rect.java
@@ -0,0 +1,125 @@
+package io.rala.math.geometry;
+
+import java.util.Objects;
+
+/**
+ * rect in 2d area
+ */
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class Rect {
+    // region attributes
+
+    private Point point;
+    private double height;
+    private double width;
+
+    // endregion
+
+    // region constructors
+
+    /**
+     * calls {@link #Rect(Point, double, double)} with {@link Point#Point()}
+     *
+     * @param height height of rect
+     * @param width  width of rect
+     */
+    public Rect(double height, double width) {
+        this(new Point(), height, width);
+    }
+
+    /**
+     * creates rect with point, height and width
+     *
+     * @param point  point of rect
+     * @param height height of rect
+     * @param width  width of rect
+     */
+    public Rect(Point point, double height, double width) {
+        this.point = point;
+        this.height = height;
+        this.width = width;
+    }
+
+    // endregion
+
+    // region getter and setter
+
+    /**
+     * @return point of rect
+     */
+    public Point getPoint() {
+        return point;
+    }
+
+    /**
+     * @param point new point of rect
+     */
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+
+    /**
+     * @return height of rect
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * @param height new height of rect
+     */
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    /**
+     * @return width of rect
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * @param width new width of rect
+     */
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    // endregion
+
+    // region copy
+
+    /**
+     * @return new rect with same values
+     */
+    public Rect copy() {
+        return new Rect(getPoint().copy(), getHeight(), getWidth());
+    }
+
+    // endregion
+
+    // region override
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rect rect = (Rect) o;
+        return Double.compare(rect.getHeight(), getHeight()) == 0 &&
+            Double.compare(rect.getWidth(), getWidth()) == 0 &&
+            Objects.equals(getPoint(), rect.getPoint());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPoint(), getHeight(), getWidth());
+    }
+
+    @Override
+    public String toString() {
+        return getPoint() + " " + getHeight() + "x" + getWidth();
+    }
+
+    // endregion
+}
---
+++ src/test/java/io/rala/math/geometry/RectTest.java
@@ -0,0 +1,85 @@
+package io.rala.math.geometry;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("SameParameterValue")
+class RectTest {
+    // region constructors, getter and setter
+
+    @Test
+    void testConstructors() {
+        assertRect(new Rect(1, 2), 1, 2);
+        assertRect(new Rect(new Point(1), 1, 2), new Point(1), 1, 2);
+    }
+
+    @Test
+    void testGetterAndSetter() {
+        Rect rect = new Rect(0, 0);
+        assertRect(rect, 0, 0);
+        rect.setPoint(new Point(1));
+        assertRect(rect, new Point(1), 0, 0);
+        rect.setHeight(2);
+        assertRect(rect, new Point(1), 2, 0);
+        rect.setWidth(3);
+        assertRect(rect, new Point(1), 2, 3);
+    }
+
+    // endregion
+
+    // region copy
+
+    @Test
+    void testCopy() {
+        Rect rect = new Rect(new Point(2), 3, 4);
+        Assertions.assertEquals(rect, rect.copy());
+    }
+
+    // endregion
+
+    // region override
+
+    @Test
+    void testEquals() {
+        Rect rect = new Rect(new Point(2), 3, 4);
+        Assertions.assertEquals(
+            rect,
+            new Rect(new Point(2), 3, 4)
+        );
+        Assertions.assertNotEquals(
+            rect,
+            new Rect(new Point(2), 4, 3)
+        );
+    }
+
+    @Test
+    void testHashCode() {
+        Assertions.assertEquals(
+            18254816,
+            new Rect(new Point(2), 3, 4).hashCode()
+        );
+    }
+
+    @Test
+    void testToString() {
+        Rect rect = new Rect(new Point(2), 3, 4);
+        Assertions.assertEquals("2.0:2.0 3.0x4.0", rect.toString());
+    }
+
+    // endregion
+
+
+    // region assert
+
+    private static void assertRect(Rect rect, double height, double width) {
+        assertRect(rect, new Point(), height, width);
+    }
+
+    private static void assertRect(Rect rect, Point point, double height, double width) {
+        Assertions.assertEquals(point, rect.getPoint());
+        Assertions.assertEquals(height, rect.getHeight());
+        Assertions.assertEquals(width, rect.getWidth());
+    }
+
+    // endregion
+}
