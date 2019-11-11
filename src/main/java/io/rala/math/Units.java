a9fe31ce4d16569f11ccfb2cb17c9ea77230b0b5
rala72 <code@rala.io>
2019-11-11 13:52:00 +0100

    add units class

---
+++ src/main/java/io/rala/math/Units.java
@@ -0,0 +1,26 @@
+package io.rala.math;
+
+/**
+ * collection of unit conversion functions
+ */
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class Units {
+    private Units() {
+    }
+
+    /**
+     * @param a grad to convert
+     * @return <code>2*&pi;/360 * a</code>
+     */
+    public static double gradToRad(double a) {
+        return (2 * Math.PI * a) / 360;
+    }
+
+    /**
+     * @param a radians to convert
+     * @return <code>360/2*&pi; * a</code>
+     */
+    public static double radToGrad(double a) {
+        return (360 * a) / (2 * Math.PI);
+    }
+}
---
+++ src/test/java/io/rala/math/testUtils/UnitsArgumentsStreamFactory.java
@@ -0,0 +1,41 @@
+package io.rala.math.testUtils;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+@SuppressWarnings("unused")
+public class UnitsArgumentsStreamFactory {
+    private UnitsArgumentsStreamFactory() {
+    }
+
+    public static Stream<Arguments> gradToRad() {
+        return Stream.of(
+            Arguments.of(-90, -Math.PI / 2),
+            Arguments.of(-45, -Math.PI / 4),
+            Arguments.of(0, 0),
+            Arguments.of(1, Math.PI / 180),
+            Arguments.of(45, Math.PI / 4),
+            Arguments.of(90, Math.PI / 2),
+            Arguments.of(135, Math.PI * 3 / 4),
+            Arguments.of(180, Math.PI),
+            Arguments.of(270, Math.PI * 3 / 2),
+            Arguments.of(360, Math.PI * 2)
+        );
+    }
+
+    public static Stream<Arguments> radToGrad() {
+        return Stream.of(
+            Arguments.of(-Math.PI / 2, -90),
+            Arguments.of(-Math.PI / 4, -45),
+            Arguments.of(0, 0),
+            Arguments.of(Math.PI / 180, 1),
+            Arguments.of(Math.PI / 4, 45),
+            Arguments.of(Math.PI / 2, 90),
+            Arguments.of(Math.PI * 3 / 4, 135),
+            Arguments.of(Math.PI, 180),
+            Arguments.of(Math.PI * 3 / 2, 270),
+            Arguments.of(Math.PI * 2, 360)
+        );
+    }
+}
