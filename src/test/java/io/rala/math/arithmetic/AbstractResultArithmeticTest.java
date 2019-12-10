c0411202de4f56b3142e242925f91cd06288cf31
rala72 <code@rala.io>
2019-12-10 20:25:54 +0100

    remove public from abstractResultArithmeticTest

--- src/test/java/io/rala/math/arithmetic/AbstractResultArithmeticTest.java
+++ src/test/java/io/rala/math/arithmetic/AbstractResultArithmeticTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class AbstractResultArithmeticTest {
+class AbstractResultArithmeticTest {
     private AbstractResultArithmetic<Number, Number> arithmetic;
 
     @BeforeEach
