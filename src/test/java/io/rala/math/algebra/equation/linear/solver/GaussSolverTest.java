38c33c42b46c9b4bb1a5ad67bd425dad760153dc
rala72 <code@rala.io>
2019-12-16 14:06:57 +0100

    add infinite equation test

--- src/test/java/io/rala/math/algebra/equation/linear/solver/GaussSolverTest.java
+++ src/test/java/io/rala/math/algebra/equation/linear/solver/GaussSolverTest.java
@@ -96,6 +96,22 @@ class GaussSolverTest {
         );
     }
 
+    @Test
+    void solveInfiniteLinearEquationSystem() {
+        LinearEquationSystem<Double> equationSystem = new LinearEquationSystem<>(
+            DoubleMatrix.ofValuesByRows(3,
+                1, 1, 2, 9,
+                0, 1, 3, 1,
+                0, 2, 6, 2
+            )
+        );
+        GaussSolver<Double> solver = new GaussSolver<>(equationSystem);
+        Assertions.assertEquals(
+            Solution.infinite(equationSystem),
+            solver.solve()
+        );
+    }
+
     // endregion
 
     // region prepare
