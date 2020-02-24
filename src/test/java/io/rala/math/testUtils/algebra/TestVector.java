3076b40db74e1994d3c8018269f1e6a33b17e10a
Sebastian Brunhofer <sebastian.brunhofer@gmail.com>
2020-02-24 10:12:09 +0100

    add more constructors for TestVector

--- src/test/java/io/rala/math/testUtils/algebra/TestVector.java
+++ src/test/java/io/rala/math/testUtils/algebra/TestVector.java
@@ -22,6 +22,21 @@ public class TestVector extends Vector<Number> {
         }
     }
 
+    public TestVector(int size, Type type) {
+        this(size, type, true);
+    }
+
+    public TestVector(int size, Type type, boolean empty) {
+        super(new TestAbstractArithmetic(), size, 0d);
+        if (!empty) {
+            for (int i = 0; i < size(); i++) {
+                setValue(i, getArithmetic().product(
+                    getArithmetic().power(i + 1, 2),
+                    getArithmetic().power(getArithmetic().fromInt(-1), i)));
+            }
+        }
+    }
+
     public TestVector(int size, double defaultValue) {
         super(new TestAbstractArithmetic(), size, defaultValue);
     }
